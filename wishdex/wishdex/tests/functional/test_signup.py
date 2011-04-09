7b8688f72ec4f3e0fc22c613b64f698104fdfcc5
singhala <singhala@mit.edu>
2011-04-09 14:03:52 -0400

    clean

--- wishdex/wishdex/tests/functional/test_signup.py
+++
@@ -1,7 +0,0 @@
-from wishdex.tests import *
-
-class TestSignupController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='signup', action='index'))
-        # Test response...
